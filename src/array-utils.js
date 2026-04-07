3355f069dbe2c533b93de803149e9721d4dfd891
Pooja-S-2006 <poojasenthil@1521>
2026-04-07 19:32:45 +0530

    Add array manipulation utilities

---
+++ src/array-utils.js
@@ -0,0 +1,34 @@
+// Array manipulation utilities
+export function chunk(array, size) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunks.push(array.slice(i, i + size));
+    }
+    return chunks;
+}
+
+export function unique(array) {
+    return [...new Set(array)];
+}
+
+export function shuffle(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
+export function groupBy(array, key) {
+    return array.reduce((groups, item) => {
+        const group = item[key];
+        groups[group] = groups[group] || [];
+        groups[group].push(item);
+        return groups;
+    }, {});
+}
+
+export function flatten(array) {
+    return array.flat(Infinity);
+}
